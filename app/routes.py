1d665ebdcf29c3f822e95e4622cad812e4f5a4d5
Connor Smallwood <mason.smallwood@bobcats.gcsu.edu>
2024-10-08 16:00:20 -0400

    1

--- app/routes.py
+++ app/routes.py
@@ -9,3 +9,19 @@ def index():
 def about():
     return render_template('about.html')
 
+@app.route('/adoptions')
+def adoptions():
+    return render_template('adoptions.html')
+
+@app.route('/products')
+def products():
+    return render_template('products.html')
+
+@app.route('/services.html')
+def services():
+    return render_template('services.html')
+
+@app.route('/help')
+def help():
+    return render_template('help.html')
+
